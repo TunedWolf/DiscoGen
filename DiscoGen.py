1cdcda76a49b3c32dfbd556e3d5fd792131c4d72
Tubekat <baternutnik@gmail.com>
2024-02-22 02:21:24 +0300

    Added Boot And Found Embeds

--- DiscoGen.py
+++ DiscoGen.py
@@ -15,10 +15,76 @@ def send_request(code):
         send_to_discord(code)
 
 def send_to_discord(code):
+    codec = code
+    code = "https://discord.gift/"+ str(code)
     """Send the code through Discord webhook."""
-    payload = {"content": f"Found one! {code}"}
+    payload = {
+  "content": "WOAH!",
+  "embeds": [
+    {
+      "title": "Found a vaild code!",
+      "description": "You're Lucky! this has a 1 in 5 **quintillion** chance of happening!",
+      "color": 8279176,
+      "footer": {
+        "text": "Go buy a lottery ticket you're lucky"
+      },
+      "image": {
+        "url": "https://preview.redd.it/4daujeiyitg51.png?width=3840&format=png&auto=webp&s=0a54edb7f7946660e0120881ea81d8483b2e4e8a"
+      },
+      "author": {
+        "name": "WOAH!!!!"
+      },
+      "fields": [
+        {
+          "name": "Heres your code:",
+          "value": ""    
+        },
+        {
+          "name": "".join(str(codec)),
+          "value":  "".join(str(code))
+        }
+      ]
+    }
+  ]
+ }
     response = requests.post(WEBHOOK_URL, json=payload)
     print(response.status_code)
+
+data = {
+  "content": "Running",
+  "embeds": [
+    {
+      "title": "DiscoGen V0.1",
+      "description": "Thanks for using [DiscoGen](https://github.com/TunedWolf/DiscoGen) ",
+      "url": "https://discordapp.com",
+      "color": 16749803,
+      "image": {
+        "url": "https://i0.wp.com/www.techarp.com/wp-content/uploads/2023/05/Discord-Nitro-free-1-month.jpg?fit=900%2C450&ssl=1"
+      },
+      "author": {
+        "name": "TunedWolf",
+        "url": "https://discordapp.com",
+        "icon_url": "https://cdn.discordapp.com/avatars/716267808983613472/b0fece70f00703d05236287bdf5ca1f5.webp?size=128"
+      },
+      "fields": [
+        {
+          "name": "What is DiscoGen?",
+          "value": "DiscoGen is a nitro code generator thats running on [Python](https://www.python.org/)"
+        },
+        {
+          "name": "How Does It Work?",
+          "value": "DiscoGen generates an 18 character alphanumeric string which it then checks if its valid with discords api if it is then its sent to this channel as a link"
+        },
+        {
+          "name": "Why?",
+          "value": "I got bored at 3AM one day and decided i wanted to make this."
+        }
+      ]
+    }
+  ]
+}
+reply = requests.post(WEBHOOK_URL, json=data)
+print(reply.status_code)
 
 while 1:
     if __name__ == "__main__": 
